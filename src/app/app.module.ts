39ceb50396505c3eec4d93e4b6efe923c715f368
kavya.pl <kavya.pl@gmail.com>
2019-04-27 19:59:12 +0530

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { ProductsComponent } from "./products/products.component";
+import { ServicesComponent } from "./services/services.component";
+import { AchievementsComponent } from "./achievements/achievements.component";
+import { ContactusComponent } from "./contactus/contactus.component";
+import { HeaderComponent } from "./header/header.component";
+
+import { RouterModule } from "@angular/router";
+import { FallbackComponent } from "./fallback/fallback.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ServicesComponent,
+    AchievementsComponent,
+    ContactusComponent,
+    HeaderComponent,
+    FallbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: HomeComponent
+        // redirectTo: "contactus",
+        // pathMatch: "full"
+      },
+      {
+        path: "services",
+        component: ServicesComponent
+      },
+      {
+        path: "products",
+        component: ProductsComponent
+      },
+      {
+        path: "achievements",
+        component: AchievementsComponent
+      },
+      {
+        path: "contactus",
+        component: ContactusComponent
+      },
+      {
+        path: "ourresults",
+        redirectTo: "achievements"
+      },
+      {
+        path: "**",
+        component: FallbackComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
